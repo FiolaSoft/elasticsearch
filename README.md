8fe3c2609cd198d3356ffd6b81fc35536a67d206
Vojtěch Mareš <ja@vojtamares.cz>
2018-04-23 17:20:14 +0200

    first commit, added ElasticSearchExtension.php, example config, basic docs in README.md, composer.json and adjusted .gitignore

---
+++ README.md
@@ -0,0 +1,35 @@
+# FiolaSoft/ElasticSearch
+
+This is integration of **elasticsearch/elasticsearch** into Nette framework.
+
+## Installation
+```yaml
+extensions:
+    elasticsearch: FiolaSoft\ElasticSearch\DI\ElasticSearchExtension
+```
+
+## Configuration
+#### Minimal configuration
+```yaml
+elasticsearch:
+    hosts:
+      - 'localhost'
+```
+#### Advanced configuration
+```yaml
+elasticsearch:
+    hosts:
+        -
+            host: 'localhost'
+            port: 9200
+            scheme: 'https'
+            user: 'foo'
+            pass: 'bar'
+```
+
+**NOTE:** The `host` is required, others are recommanded, but not necessary.
+
+
+Find more about [elasticsearch/elasticsearch](https://github.com/elastic/elasticsearch-php).
+
+And there are [ElasticSearch docs](https://www.elastic.co/guide/en/elasticsearch/client/php-api/5.0/index.html)
---
+++ src/FiolaSoft/ElasticSearch/DI/ElasticSearchExtension.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+
+namespace FiolaSoft\ElasticSearch\DI;
+
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
+use Nette\DI\CompilerExtension;
+
+class ElasticSearchExtension extends CompilerExtension
+{
+    /**
+     * @var array
+     */
+    public $defaults = [
+            'hosts'      =>  [
+                    'localhost'
+            ],
+
+            /*
+            This is effectively equal to: "https://username:password!#$?*abc@foo.com:9200/"
+            [
+                    'host' => 'foo.com',
+                    'port' => '9200',
+                    'scheme' => 'https',
+                    'user' => 'username',
+                    'pass' => 'password!#$?*abc'
+            ],
+            */
+    ];
+
+    public function beforeCompile(): void
+    {
+        $config = $this->getConfig($this->defaults);
+
+        $builder = $this->getContainerBuilder();
+
+        $builder->addDefinition($this->prefix('clientBuilder'))
+                ->setClass(ClientBuilder::class)
+                ->setFactory([ClientBuilder::class, 'create'])
+                ->setArguments($config['hosts']);
+
+        $builder->addDefinition($this->prefix('client'))
+                ->setClass(Client::class)
+                ->setFactory(['@' . $this->prefix('clientBuilder'), 'build']);
+    }
+
+}
